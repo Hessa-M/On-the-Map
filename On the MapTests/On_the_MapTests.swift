84e9a7b54a2330c1a9b651290b62217ef5f745f0
Hessa <hessa.m.muhanna@gmail.com>
2018-12-17 14:20:13 +0300

    Initial Commit

---
+++ On the MapTests/On_the_MapTests.swift
@@ -0,0 +1,34 @@
+//
+//  On_the_MapTests.swift
+//  On the MapTests
+//
+//  Created by Hessa Mohamed on 17/12/2018.
+//  Copyright © 2018 Hessa Mohamed. All rights reserved.
+//
+
+import XCTest
+@testable import On_the_Map
+
+class On_the_MapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
