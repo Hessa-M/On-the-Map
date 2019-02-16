e43d7f1172c23ccf4441cb98865a151205752273
Hessa <hessa.m.muhanna@gmail.com>
2019-02-16 11:18:31 +0300

    first commit

---
+++ On the Map/View/LocationTableCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableCell.swift
+//  On the Map
+//
+//  Created by Hessa Mohamed on 19/01/2019.
+//  Copyright © 2019 Hessa Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+}
---
+++ On the Map/View/TestLocationTableCell.swift
@@ -0,0 +1,9 @@
+//
+//  TestLocationTableCell.swift
+//  Example12
+//
+//  Created by Hessa Mohamed on 19/01/2019.
+//  Copyright © 2019 Ammar AlTahhan. All rights reserved.
+//
+
+import Foundation
